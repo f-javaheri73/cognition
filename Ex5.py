eb451a46debce321366708e30a30e5a6b0a6c329
f-javaheri73 <63813959+f-javaheri73@users.noreply.github.com>
2020-04-22 06:57:16 -0700

    Add files via upload

---
+++ Ex5.py
@@ -0,0 +1,30 @@
+
+
+2"""def  func():
+    x = int(input('yek adad vared konid:'))
+    for i in range(x,-1,-1):
+     print(i)"""
+
+
+3"""def func():
+    num1 = int(input('adad vared konid:'))
+    num2 = int(input('adad vared konid:'))
+    if num1% num2 == 0:
+        print('bakhsh pazir ast')
+    else:
+        print('bakhsh pazir nist')"""
+    
+4"""def func():
+    num = int(input('adad vared konid:'))
+    if num>1:
+        for i in range(2,num):
+            if(num%i)==0:
+                print(num,"adad aval nist")
+                print(i,"zarbdar", num//i,"mishavad",num)
+                break
+            else:
+                print(num,"adade aval hast")
+
+        else:
+            print(num,"adae aval nist")"""
+    
