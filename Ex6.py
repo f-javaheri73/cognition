b5c39829f4d328a6de10bd2e7109e577dd544d8e
f-javaheri73 <63813959+f-javaheri73@users.noreply.github.com>
2020-04-29 02:57:29 -0700

    Add files via upload

---
+++ Ex6.py
@@ -0,0 +1,27 @@
+#06-1
+"""def fib (n):
+    if n<0: 
+        print("adad eshtebah ast") 
+    elif n==1: 
+        return 0
+    elif n==2: 
+        return 1
+    else: 
+        return fib(n-1)+fib(n-2)"""
+
+#06-2
+
+"""def fib(n):  
+   if n <= 1:  
+       return n  
+   else:  
+       return(fib(n-1) + fib(n-2))  
+ 
+jomle = int(input("chand jomle?: "))  
+ 
+if jomle <= 0:  
+   print("adad mosbat vared konid: ")  
+else:  
+   print(" donbale Fibonacci :")  
+   for i in range(jomle):  
+       print(fib(i))"""
