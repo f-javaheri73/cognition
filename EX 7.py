afb1bb46c7e10457296422665b5d9042e6736c8d
f-javaheri73 <63813959+f-javaheri73@users.noreply.github.com>
2020-04-29 00:33:38 -0700

    Add files via upload

---
+++ EX 7.py
@@ -0,0 +1,81 @@
+"""
+#012
+first = int(input("enter a number: "))
+second = int(input("enter second number: "))
+if first > second:
+    print(str(second) +"\n" + str(first))
+else:
+    print(str(first) + "\n" + str(second))
+"""
+"""
+#013
+num = int(input("enter a number under 20 : "))
+if num >= 20:
+    print("too high")
+else:
+    print("thnaks")
+"""
+"""
+#014
+num = int(input("enter a number between 10 and 20 : "))
+if 20>=num >=10:
+    print ("thanks")
+else:
+    print("incorrect answer")
+"""
+"""
+#015
+color = input("Enter your favorite color : ")
+if color == "Red" or color == "RED" or color == "red":
+    print("i like red too")
+else:
+    print("i don't like " + color + " i prefer Red")
+"""
+"""
+#016
+weather = input("is it raining?")
+weather = weather.lower()
+if weather == "yes":
+    windy = input("is it windy? ")
+    windy == windy.lower()
+    if windy == "yes":
+        print("it is too windy for an umbrella")
+    else:
+        print("take an umbrella")
+else:
+    print("enjoy your day")
+"""
+"""
+#017
+age = int(input("waht is your age? "))
+if age >= 18:
+    print("you can vote")
+elif age >=17:
+    print("you can learn to drive")
+elif age >= 16:
+    print("you can buy a lottery ticket")
+else:
+    print("you can go trick-or-treating")
+"""
+"""
+#018
+number = int(input("enter a number: "))
+if number < 10:
+    print("too low")
+elif 20 >number> 10:
+    print("correct")
+else:
+    print("too high")
+"""
+"""
+#019
+number = int(input("enter a number between 1,2,3 : "))
+if number == 1:
+    print("thank you")
+elif number == 2:
+    print("well done")
+elif number == 3:
+    print("correct")
+else:
+    print("Error message")
+"""
