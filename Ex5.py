e2567a541626ef6a3b734d2846192444f6f51623
f-javaheri73 <63813959+f-javaheri73@users.noreply.github.com>
2020-05-07 03:31:42 -0700

    Update Ex5.py

--- Ex5.py
+++ Ex5.py
@@ -1,5 +1,27 @@
+1"""days = [31,28,31,30,31,30,31,31,30,31,30,31]
 
 
+
+def julian_date(d,m,y):
+    """ this function take day, month and year, and return days passed from the beginning"""
+   
+    if y%400==0 or (y%4 ==0 and y%100 != 0):
+        days[1]=29
+        days_passed = 0
+        for items in days[:m-1]:
+            
+
+
+            days_passed += items 
+            
+
+            
+
+    days_passed +=d
+    print(days_passed)"""
+            
+            
+            
 2"""def  func():
     x = int(input('yek adad vared konid:'))
     for i in range(x,-1,-1):
