887ba029083742d8be4e096c0084f572cbc393d2
f-javaheri73 <63813959+f-javaheri73@users.noreply.github.com>
2020-05-07 18:26:36 +0430

    Add files via upload

---
+++ self-Ex1.py
@@ -0,0 +1,36 @@
+# tamrine ruzhaye gozashte ba estefade az sum
+
+
+days = [31,28,31,30,31,30,31,31,30,31,30,31]
+
+def julian_date(d,m,y):
+    if y%400 ==0 or (y%4 ==0 and y%100 !=0):
+        
+        
+        days[1] = 29
+        sum(days[:m-1])
+        dd = sum(days[:m-1])
+        
+        final_d = dd+d
+        print(final_d)
+    elif y%400 !=0 or (y%4 ==0 and y%100 ==0):
+        
+        sum(days[:m-1])
+        dd = sum(days[:m-1])
+        
+        final_d = dd+d
+        return final_d
+
+
+    
+            
+ 
+   
+   
+            
+            
+            
+            
+            
+    
+ 
